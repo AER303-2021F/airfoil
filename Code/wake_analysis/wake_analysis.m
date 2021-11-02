7d1d2d0cd26e489ba1c3200d564a72e920c2dc47
Mingde Yin <54449457+itchono@users.noreply.github.com>
2021-11-02 14:56:58 -0400

    New calibration!!!!!

--- Code/wake_analysis/wake_analysis.m
+++ Code/wake_analysis/wake_analysis.m
@@ -2,6 +2,13 @@
 load("alpha_0_wake_pressure_test.mat")
 load("../../Data/Calibrated_Rake_Pa.mat")
 alphas = [0 3 6 8 10 11 13 15 16 17 20];
+
+figure
+hold on
+
+xlabel("Velocity Deficit (m/s)", "interpreter", "latex")
+ylabel("Vertical Position (m)", "interpreter", "latex")
+
 for i = 1:11
     alpha = alphas(i);
     RHO = 1.225; % kg/m^3
@@ -10,14 +17,13 @@ for i = 1:11
 
     combined_locations = [locations_b; locations_a]; % indices in ascending order
     combined_locations = combined_locations(:) / 100; % locations in m
-    combined_rakes = [eval(sprintf("p_rakeb_%d", alpha))'; eval(sprintf("p_rakea_%d", alpha))'];
+    combined_rakes = [eval(sprintf("p_rakeb_%d", alpha)); eval(sprintf("p_rakea_%d", alpha))];
     combined_rakes = combined_rakes(:);
     velocities = sqrt(2 * combined_rakes / RHO);
 
-%     plot(V_mps - velocities, combined_locations)
-%     title("Wake Velocity Profile for $\alpha$ = " + sprintf("%d", alpha) + "$^\circ$", "interpreter", "latex")
-%     xlabel("Velocity Deficit (m/s)", "interpreter", "latex")
-%     ylabel("Vertical Position (m)", "interpreter", "latex")
+    plot(V_mps - velocities, combined_locations)
+    %title("Wake Velocity Profile for $\alpha$ = " + sprintf("%d", alpha) + "$^\circ$", "interpreter", "latex")
+    
 
     drag = RHO * trapz(combined_locations,  velocities .* (V_mps - velocities));
     c_D = drag / (1/2 * RHO * V_mps^2 * 0.1);
