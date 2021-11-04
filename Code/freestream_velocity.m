029866f2a8b6a1c67c09fcdd61e691b743f2e945
Mingde Yin <54449457+itchono@users.noreply.github.com>
2021-11-04 01:46:55 -0400

    Freestream velocity calculation and error

---
+++ Code/freestream_velocity.m
@@ -0,0 +1,32 @@
+% Calculation of free-stream velocity for each AoA
+
+load("../Data/calibrated_manometer_data_Pa.mat");
+load("../Data/Calibrated_Rake_Pa.mat");
+% Load manometer and scanivalve pressure data
+RHO = 1.225;
+d_p_mano = 10;
+% Manometer error, Pa
+d_p_scani = 9.80665 * 0.0063;
+% Maximum error from all scanivalve time series, Pa
+
+alphas = [0 3 6 8 10 11 13 15 16 17 20];
+v_inf_scanivalve = zeros(size(alphas));
+d_v_inf_scanivalve = zeros(size(alphas));
+v_inf_manometer = zeros(size(alphas));
+d_v_inf_manometer = zeros(size(alphas));
+
+for i = 1:11
+    alpha = alphas(i);
+    combined_rakes_scanivalve = [eval(sprintf("p_rakeb_%d", alpha)); eval(sprintf("p_rakea_%d", alpha))];
+    combined_rakes_scanivalve = combined_rakes_scanivalve(:);
+    v_inf_scanivalve(i) = 1/2 * (sqrt(2 * combined_rakes_scanivalve(1) / RHO) + sqrt(2 * combined_rakes_scanivalve(end) / RHO));
+    d_v_inf_scanivalve(i) = sqrt((d_p_scani/(2*v_inf_scanivalve(i)*RHO))^2 + (d_p_scani/(2*v_inf_scanivalve(i)*RHO))^2);
+    
+    combined_rakes_manometer = [rake_b_manometer(i,:); rake_a_manometer(i,:)];
+    combined_rakes_manometer = combined_rakes_manometer(:);
+    v_inf_manometer(i) = 1/2 * (sqrt(2 * combined_rakes_manometer(1) / RHO) + sqrt(2 * combined_rakes_manometer(end) / RHO));
+    d_v_inf_manometer(i) = sqrt((d_p_mano/(2*v_inf_manometer(i)*RHO))^2 + (d_p_mano/(2*v_inf_manometer(i)*RHO))^2);
+    
+    writematrix([v_inf_manometer; d_v_inf_manometer], "../Data/velocities_manometer.csv");
+    writematrix([v_inf_scanivalve; d_v_inf_scanivalve], "../Data/velocities_scanivalve.csv");
+end
